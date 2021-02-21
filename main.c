4ed31dc5e5ce1159bdf8721b913a3ac1e90983e5
veronicamars73 <mendie73@gmail.com>
2021-02-21 15:56:18 -0300

    Removendo sync

--- main.c
+++ main.c
@@ -16,17 +16,15 @@ double funcao_geo(double grau_rad);
 
 // Variáveis globais
 int index_global = 0;
-double* vetor;
+double** vetor;
+double area;
 
 // Struct de parâmetros para as funções
 typedef struct  {
-	double ponto_inicial;
-	double ponto_final;
-	int num_trapezios;
-	int num_threads;
-	int id_thread;
+	double local_n;
+	double local_a;
 	double altura_trapezios;
-	pthread_t *threads;
+	int num_threads;
 }InfosIntegral;
  
 int main (int argc, char *argv[]) {
@@ -38,9 +36,9 @@ int main (int argc, char *argv[]) {
 		return uso();
 	}
 
-	double ponto_inicial, ponto_final, somatorio_area = 0.0;
+	double ponto_inicial, ponto_final, altura_trapezios;
+	double somatorio_area = 0.0;
 	int num_trapezios, num_threads, id_funcao;
-	InfosIntegral info_obj;
 
 	sscanf(argv[1], "%lf", &ponto_inicial);
 	sscanf(argv[2], "%lf", &ponto_final);
@@ -48,39 +46,54 @@ int main (int argc, char *argv[]) {
 	sscanf(argv[4], "%d", &num_threads);
 	sscanf(argv[5], "%d", &id_funcao);
 
-	// Alocação dinâmica do vetor global
-	vetor = (double*) calloc(num_threads, sizeof(double));
-
-	// Atribuição dos valores das variáveis do struct com os parâmetros da linha de comando
-	info_obj.ponto_inicial = ponto_inicial;
+	InfosIntegral * info_obj = (InfosIntegral *) malloc(num_threads * sizeof(InfosIntegral));
+	// Acrescentando o pi no final da função 2
 	if(id_funcao == 2){
-		info_obj.ponto_final = ponto_final * acos(-1.0);
-	}else{
-		info_obj.ponto_final = ponto_final;
+		ponto_final = ponto_final * acos(-1.0);
 	}
-	info_obj.num_threads = num_threads;
-	info_obj.num_trapezios = num_trapezios;
-	info_obj.altura_trapezios = (info_obj.ponto_final - ponto_inicial) / (num_trapezios);
+
+	altura_trapezios = (ponto_final - ponto_inicial) / (num_trapezios);
+
+	int i = 0;
+	while(1){
+		if(i==num_threads){
+			break;
+		}
+		if(i==0){
+			info_obj[i].local_n = num_trapezios / num_threads + (num_trapezios % num_threads);
+			info_obj[i].local_a = ponto_inicial;
+		}else{
+			info_obj[i].local_n = num_trapezios / num_threads;
+			info_obj[i].local_a = ponto_inicial + (info_obj[i].local_n * i * altura_trapezios) +
+			((num_trapezios % num_threads) * altura_trapezios);
+		}
+		info_obj[i].altura_trapezios = altura_trapezios;
+		info_obj[i].num_threads = num_threads;
+		i++;
+	}
+	// Alocação dinâmica do vetor global
+	vetor = (double**) calloc(num_threads, sizeof(double*));
+
+	for (int i = 0; i < num_threads; ++i) {
+		vetor[i] = NULL;
+	}
 
 
 	pthread_t threads[num_threads];
-	info_obj.threads = threads;
 
 	// Laços para a criação de threads sincronizadas passando o struct como parâmetro
 	if(id_funcao == 1){ // Função 1: f(x) = 5
 		for(int i = 0 ; i < num_threads; ++i){
-			info_obj.id_thread = i;
-			printf("Criando thread %d...\n", i);
-			pthread_create(&threads[i], NULL, funcao_id1, &info_obj);
-			pthread_join(threads[i],NULL);
+
+			pthread_create(&threads[i], NULL, funcao_id1, &info_obj[i]);
+			//pthread_join(threads[i],NULL);
 		}
 	}else{
 		if (id_funcao == 2) { // Função 2: f(x) = sen(x*2) + cos(x*5)
 			for(int i = 0 ; i < num_threads; ++i){
-				info_obj.id_thread = i;
-				printf("Criando thread %d...\n", i);
-				pthread_create(&threads[i], NULL, funcao_id2, &info_obj);
-				pthread_join(threads[i],NULL);
+
+				pthread_create(&threads[i], NULL, funcao_id2, &info_obj[i]);
+				//pthread_join(threads[i],NULL);
 			}
 		}else {
 			printf("Código de função inválido!\n");
@@ -88,9 +101,13 @@ int main (int argc, char *argv[]) {
 		}
 	}
 
+	for (int i = 0; i < num_threads; ++i){
+		pthread_join(threads[i], NULL);
+	}
+	
 	// Calcula o somatório das áreas
 	for(int i = 0; i < num_threads; ++i){
-		somatorio_area += vetor[i];
+		somatorio_area += *(vetor[i]);
 	}
 
 	//printf("O valor total da área é: %1.2e\n", somatorio_area);
@@ -98,6 +115,7 @@ int main (int argc, char *argv[]) {
 
 	// Libera o espaço de memória alocado dinamicamente
 	free(vetor);
+	free(info_obj);
 
 	return 0;  
 }
@@ -113,36 +131,19 @@ int uso(){
 void *funcao_id1(void *argumentos) {
 
 	InfosIntegral * ifobj = (InfosIntegral *) argumentos;
-	double area;
-	int local_n;
-
-	// Verifica se não é a primeira thread
-	if (ifobj->id_thread != 0){ 
-		
-		// Calcula o número de trapézios da thread
-		local_n = ifobj->num_trapezios / ifobj->num_threads;
-		
-		// Calcula a área do intervalo
-		area = (5+5)*ifobj->altura_trapezios/2; // [f(local_a)+f(local_a+h)]*(altura/2)
-		for (int i = 1; i < local_n; ++i){
-			area += (5+5)*ifobj->altura_trapezios/2;// [f(prox_a)+f(prox_a+h)]*(altura/2)
-		}
-
-	}else{
-		// Na primeira thread, local_n será somada ao resto da divisão para cobrir os casos de divisão não inteira
-		local_n = ifobj->num_trapezios / ifobj->num_threads + (ifobj->num_trapezios % ifobj->num_threads);
-		
-		// Calcula a área do intervalo
-		area = (5+5)*ifobj->altura_trapezios/2; // [f(local_a)+f(local_a+h)]*(altura/2)
-		for (int i = 1; i < local_n; ++i){
-			area += (5+5)*ifobj->altura_trapezios/2;// [f(prox_a)+f(prox_a+h)]*(altura/2)
-		}
+	area = 0;
+	area = (5+5)*ifobj->altura_trapezios/2; // [f(local_a)+f(local_a+h)]*(altura/2)
+	for (int i = 1; i < ifobj->local_n; ++i){
+		area += (5+5)*ifobj->altura_trapezios/2;// [f(prox_a)+f(prox_a+h)]*(altura/2)
 	}
 
-	printf("Thread %d em andamento, area calculada = %lf\n\n", ifobj->id_thread, area);
-
 	// Insere área calculada pela thread no vetor dinamicamente alocado
-	vetor[index_global] = area;
+	for (int i = 0; i < ifobj->num_threads; ++i) {
+		if (vetor[i]==NULL){
+
+			vetor[i] = &area;
+		}
+	}
 	index_global++;
 
 	pthread_exit(NULL);
@@ -158,43 +159,25 @@ double funcao_geo(double grau_rad){
 void *funcao_id2(void *argumentos) {
 
 	InfosIntegral * ifobj = (InfosIntegral *) argumentos;
-	double local_a, prox_a, area;
-	int local_n;
-
-	if (ifobj->id_thread != 0){
-		
-		// Calcula o número de trapézios da thread
-		local_n = ifobj->num_trapezios / ifobj->num_threads;
-
-		// Calcula o ponto incial da thread atual
-		local_a = ifobj->ponto_inicial + (local_n * ifobj->id_thread*ifobj->altura_trapezios) + (ifobj->num_trapezios % ifobj->num_threads * ifobj->altura_trapezios);
-
-		// Calcula a área do intervalo
-		area = (funcao_geo(local_a)+funcao_geo(local_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2; //f(local_a)+f(local_a+h)/2
-		for (int i = 1; i < local_n; ++i){
-			prox_a = local_a + i*ifobj->altura_trapezios;
-			area += (funcao_geo(prox_a) + funcao_geo(prox_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2;//+= (f(prox_a)+f(prox_a+h))*altura/2
-		}
-
-	}else{
-		// Na primeira thread, local_n será somada ao resto da divisão para cobrir os casos de divisão não inteira
-		local_n = ifobj->num_trapezios / ifobj->num_threads + (ifobj->num_trapezios % ifobj->num_threads);
-		
-		// Calcula  o ponto inical da thread
-		local_a = ifobj->ponto_inicial;
-		
-		// Calcula a área do intervalo
-		area = (funcao_geo(local_a)+funcao_geo(local_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2; // f(local_a)+f(local_a+h)/2
-		for (int i = 1; i < local_n; ++i){
-			prox_a = local_a+i*ifobj->altura_trapezios;
-			area += (funcao_geo(prox_a)+funcao_geo(prox_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2;//+= (f(prox_a)+f(prox_a+h))*altura/2
-		}
+	double prox_a;
+	area = 0;
+	area += (funcao_geo(ifobj->local_a)+funcao_geo(ifobj->local_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2; //f(local_a)+f(local_a+h)/2
+	for (int i = 1; i < ifobj->local_n; ++i){
+		prox_a = ifobj->local_a + i*ifobj->altura_trapezios;
+		area += (funcao_geo(prox_a) + funcao_geo(prox_a + ifobj->altura_trapezios)) * ifobj->altura_trapezios/2;//+= (f(prox_a)+f(prox_a+h))*altura/2
 	}
 
-	printf("Thread %d em andamento, area calculada = %lf\n\n", ifobj->id_thread, area);
+	printf("Thread %d em andamento, area calculada = %lf\n\n", index_global, area);
 
 	// Insere área calculada pela thread no vetor dinamicamente alocado
-	vetor[index_global] = area;
+
+	for (int i = 0; i < ifobj->num_threads; ++i) {
+		if (vetor[i]==NULL){
+
+			vetor[i] = &area;
+			area = 0;
+		}
+	}
 	index_global++;
 
 	pthread_exit(NULL);
