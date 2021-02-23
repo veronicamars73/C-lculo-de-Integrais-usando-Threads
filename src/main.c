64973ed8934982c9004c259b7294395b3de39605
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2021-02-23 12:35:06 -0300

    retira inclusao de bibliotecas desnecessarias

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>       
 #include <pthread.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
 #include <stdlib.h>
 #include <math.h>
   
